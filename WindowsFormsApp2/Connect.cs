b9c9b4a45d2a7d36768d18655d5334a99b94d10c
BabakAdam <babaka@kkszki.hu>
2024-12-20 10:46:33 +0100

    Add project files.

---
+++ WindowsFormsApp2/Connect.cs
@@ -0,0 +1,49 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+
+internal class Connect
+{
+    public MySqlConnection Connection;
+    public string Host;
+    public string DataBase;
+    public string User;
+    public string Password;
+    public string ConnectionString;
+
+    public Connect()
+    {
+        Host = "localhost";
+        DataBase = "meseljmar";
+        User = "root";
+        Password = "";
+
+        ConnectionString = "SERVER=" + Host + ";DATABASE=" + DataBase + ";UID=" + User + ";PASSWORD=" + Password + ";SslMode=None";
+        Connection = new MySqlConnection(ConnectionString);
+    }
+
+    public void OpenConnection()
+    {
+        if (Connection.State == System.Data.ConnectionState.Closed)
+        {
+            Connection.Open();
+        }
+    }
+
+    public void CloseConnection()
+    {
+        if (Connection.State == System.Data.ConnectionState.Open)
+        {
+            Connection.Close();
+        }
+    }
+
+    public MySqlDataReader GetDataByFirstName(string firstName)
+    {
+        OpenConnection();
+        string query = "SELECT * FROM data WHERE FirstName = @FirstName";
+        MySqlCommand cmd = new MySqlCommand(query, Connection);
+        cmd.Parameters.AddWithValue("@FirstName", firstName);
+
+        return cmd.ExecuteReader();
+    }
+}
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace nem
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "SERVER=localhost;DATABASE=meseljmar;UID=root;PASSWORD=;SslMode=None";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string firstName = textBox1.Text;
+            string lastName = textBox2.Text;
+
+            if (string.IsNullOrWhiteSpace(firstName) && string.IsNullOrWhiteSpace(lastName))
+            {
+                MessageBox.Show("Adjon meg egy keresztnevet vagy vezetéknevet!");
+                return;
+            }
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string query = "SELECT * FROM data WHERE FirstName LIKE @FirstName OR LastName LIKE @LastName";
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@FirstName", "%" + firstName + "%");
+                        cmd.Parameters.AddWithValue("@LastName", "%" + lastName + "%");
+
+                        MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                        DataTable dt = new DataTable();
+                        adapter.Fill(dt);
+
+                        if (dt.Rows.Count > 0)
+                        {
+                            string result = "Talált adatok:\n\n";
+                            foreach (DataRow row in dt.Rows)
+                            {
+                                result += $"ID: {row["Id"]}, Név: {row["FirstName"]} {row["LastName"]}, Jelszó: {row["Password"]}\n";
+                            }
+                            MessageBox.Show(result);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Nincs találat.");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hiba történt: " + ex.Message);
+            }
+        }
+    }
+}
